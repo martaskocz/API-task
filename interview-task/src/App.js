e08378556ae7315c7faee357d2806655d1c0a505
martaskocz <skoczykloda.mar@gmail.com>
2021-08-25 13:22:06 +0200

    useEffect (1)

--- interview-task/src/App.js
+++ interview-task/src/App.js
@@ -1,35 +1,50 @@
-import './App.css';
+import React, { useState, useEffect, useRef } from 'react';
 
-function UserList(props) {
-  return (
-      <table width={'100%'}>
-        <thead>
-        <tr>
-          <th>image</th>
-          <th>name</th>
-          <th>surname</th>
-          <th>country</th>
-          <th>delete</th>
-        </tr>
-        </thead>
-        <tbody>
+const App = () => {
 
-        </tbody>
-      </table>
-  );
-}
+  // EXAMPLE
+  // const [timer, setTimer] = useState(0);
+  // useEffect(()=>{
+  //   const interval = setInterval(()=>setTimer(currenttimer => currenttimer +1), 1000)
+  //   console.log(interval);
+  //   return () => clearInterval(interval);
+  // }, []);
+  // return <div>{timer}</div>
 
 
-function App() {
-  return (
-      <div className="App">
-        <h1>Interview task</h1>
-        <button>Colored rows</button>
-        <button>Sort by country</button>
-        <button>Restore the init state</button>
-        <UserList />
-      </div>
-  );
-}
+  const [toggle, setToggle] = useState(false);
+  const handleToggle = () => setToggle(!toggle);
+  return(
+    <Toggler toggle={toggle} onToggle={handleToggle}/>
+  )
+};
+
+const Toggler = ({toggle, onToggle}) => {
+
+  const [title, setTitle] = useState('');
+  const calledOnce = useRef(false);
+
+  useEffect(()=> {
+    if (calledOnce.current){
+      return;
+    }
+    if (toggle === true){
+      console.log('I run only here');
+      calledOnce.current = true;
+    }
+  }, [toggle]);
+
+  const handleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  return(
+    <div>
+      <input type="text" value={title} onChange={handleChange}/>
+      <button onClick={onToggle}>Toggle</button>
+      {toggle && <p>Hi there {title}!</p>}
+    </div>
+  )
+};
 
 export default App;
